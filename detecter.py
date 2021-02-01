4fd3c9db72037dbe403430ea3a12e98279aa56e5
StuGRua <stug_iii@foxmail.com>
2021-02-01 15:27:06 +0800

    Initial commit

---
+++ detecter.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+from sender import sender
+
+def GetPageInfo1():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 '
+                      'Safari/537.36 '
+    }
+    page = requests.get('https://ssdut.dlut.edu.cn/index/bkstz.htm', headers)
+    PageContent = BeautifulSoup(page.content, 'html.parser')
+    newestHref = str(PageContent.find_all('a',class_='c56628',attrs={ 'target':"_blank"}))
+    # newestHref = page.text
+    # print(newestHref)
+    with open('0'+'last.txt','r',encoding='utf-8') as file:
+        lastHref = file.read()
+        # print(lastHref)
+    file.close()
+    if lastHref != newestHref :
+        print('Detected!')
+        sender('stug_iii@foxmail.com','https://ssdut.dlut.edu.cn/index/bkstz.htm')
+        with open('0'+'last.txt','w',encoding='utf-8') as file:
+            file.write(newestHref)
+            file.close()
+    print( '0 Completed')
+
+def GetPageInfo2():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 '
+                      'Safari/537.36 '
+    }
+    page = requests.get('http://dutdice.dlut.edu.cn/xszcq/jztz/jztz.htm', headers)
+    PageContent = BeautifulSoup(page.content, 'html.parser')
+    newestHref = str(PageContent.select('li > a'))
+    # newestHref = page.text
+    # print(newestHref)
+    with open('1'+'last.txt','r',encoding='utf-8') as file:
+        lastHref = file.read()
+        # print(lastHref)
+    file.close()
+    if lastHref != newestHref :
+        print('Detected!')
+        sender('stug_iii@foxmail.com','http://dutdice.dlut.edu.cn/xszcq/jztz/jztz.htm')
+        with open('1'+'last.txt','w',encoding='utf-8') as file:
+            file.write(newestHref)
+            file.close()
+    print( '1 Completed')
+
+
+if __name__ == "__main__":
+    GetPageInfo1()
+    GetPageInfo2()
